b8a49639cf4af080f708d4baf6fa703296f76e19
Raghu Betina <raghu@firstdraft.com>
2018-11-24 21:38:56 +0000

    Whitelist columns in Performance for ActiveAdmin

--- app/admin/performances.rb
+++ app/admin/performances.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Performance do
+
+ permit_params :workout_id, :reps, :sets, :weight, :exercise_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
